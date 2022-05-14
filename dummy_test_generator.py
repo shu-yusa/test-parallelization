f037a006c23cdac4ea51d82a2c3a8f58f1a751ef
Shusaku Yusa <shu.yusa@gmail.com>
2022-05-14 09:41:08 +0900

    remove redundant code

--- dummy_test_generator.py
+++ dummy_test_generator.py
@@ -2,7 +2,7 @@ import os
 import random
 
 
-def create_dummy_test_classes(directory, num_classes, max_test_cases_per_class):
+def create_dummy_test_files(directory, num_classes, max_test_cases_per_class):
     """
     Generate dummy test class files.
     :param directory:
@@ -10,15 +10,13 @@ def create_dummy_test_classes(directory, num_classes, max_test_cases_per_class):
     :param max_test_cases_per_class:
     :return:
     """
-    test_classes = []
     for n in range(num_classes):
         with open(os.path.join(directory, "test_class_{}.py".format(n)), "w") as f:
             content = "import unittest\n\n\nclass {}(unittest.TestCase):\n".format("Dummy" + str(n) + "Test")
             for i in range(random.randint(1, max_test_cases_per_class)):
                 content += "    def test_{}(self):\n        self.assertTrue(True)\n\n".format(i)
             f.write(content)
-    return test_classes
 
 
 if __name__ == '__main__':
-    create_dummy_test_classes("tests", 100, 20)
+    create_dummy_test_files("tests", 100, 20)
